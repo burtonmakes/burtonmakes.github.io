fcea7f40cecb061c1ef45419e919269ec12d338e
Alex Burton <82059352+CocoHusky@users.noreply.github.com>
2026-07-13 00:54:39 -0500

    Restore Cocometric modern GLB with original backup

---
+++ src/data/cocometric-model-backup/original-2026-07-12/part-5.js
@@ -0,0 +1,8 @@
+import part0 from "./part-5-0.js";
+import part1 from "./part-5-1.js";
+import part2 from "./part-5-2.js";
+import part3 from "./part-5-3.js";
+import part4 from "./part-5-4.js";
+import part5 from "./part-5-5.js";
+
+export default [part0, part1, part2, part3, part4, part5].join("");
